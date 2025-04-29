00a7618056a61add2030a09d5b60bca75456204c
chfarouk <149023028+chfarouk@users.noreply.github.com>
2025-04-29 11:10:03 -0400

    Update database.py

--- api/loan-service/database.py
+++ api/loan-service/database.py
@@ -1,12 +1,11 @@
-# api/loan-service/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 
-instance_path = os.path.join(os.path.dirname(__file__), 'instance') # Chemin vers le dossier 'instance'
-os.makedirs(instance_path, exist_ok=True) # Crée le dossier si besoin
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(instance_path, 'loan_service.db')}" # URL de connexion à la base de données SQLite 
+instance_path = os.path.join(os.path.dirname(__file__), 'instance') 
+os.makedirs(instance_path, exist_ok=True) 
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(instance_path, 'loan_service.db')}" 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
